81a67b7436d6da41a32f38580765fd3e23f12c60
Ernesto J Ballon <37639825+ernestojballon@users.noreply.github.com>
2022-02-22 11:44:46 -0500

    setting up lerna

---
+++ packages/mf-store/webpack.config.js
@@ -0,0 +1,14 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "ess",
+    projectName: "mf-store",
+    webpackConfigEnv,
+    argv,
+  });
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
